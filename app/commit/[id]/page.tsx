ac7790c5bb6f655962094dcd73d42660f5e20433
revmiller <revmiller@pm.me>
2024-11-20 23:08:04 +0100

    fix: commit/[id] page

--- app/commit/[id]/page.tsx
+++ app/commit/[id]/page.tsx
@@ -2,9 +2,10 @@ interface PageProps {
 	params: {
 		id: string
 	}
+	searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export default function Page({ params }: PageProps) {
+export default function Page({ params, searchParams }: PageProps) {
 	return (
 		<div className="p-6">
 			<h1 className="text-2xl font-bold mb-4">Commit {params.id}</h1>
@@ -12,3 +13,4 @@ export default function Page({ params }: PageProps) {
 		</div>
 	)
 }
+

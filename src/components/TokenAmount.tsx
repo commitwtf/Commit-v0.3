003b7d0ffb03a8e15832c4ca3deeba16ae732d4b
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-11-26 09:47:38 +0100

    Hide TokenAmount if value is not set

--- src/components/TokenAmount.tsx
+++ src/components/TokenAmount.tsx
@@ -3,6 +3,7 @@ import { useToken } from '@/hooks/useToken'
 
 export function TokenAmount(props: { formatted: string; value: bigint; token: Address }) {
   const token = useToken(props.token)
+  if (typeof props.value === 'undefined') return null
   const value = formatUnits(props.value, token.data?.decimals ?? 18)
 
   return (

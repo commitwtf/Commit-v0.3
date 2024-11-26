b805a89c644fb02f0dc62224699b23898cb99008
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-11-26 20:37:56 +0100

    Fix timestamps

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,6 +4,15 @@ export const toNow = (date?: string | number, opts = { addSuffix: true }) =>
   date ? formatDistanceToNow(date, opts) : undefined
 
 export function formatSecondsToDays(seconds = 0) {
-  const duration = intervalToDuration({ start: 0, end: seconds })
-  return formatDuration(duration, { format: ['days', 'hours', 'minutes'] })
+  const now = new Date()
+  const targetDate = new Date(seconds)
+  if (now > targetDate) return '--'
+  const duration = intervalToDuration({ start: now, end: targetDate })
+
+  const formattedDuration = formatDuration(duration, {
+    delimiter: ', ',
+    format: ['days', 'hours', 'minutes'],
+  })
+
+  return formattedDuration
 }

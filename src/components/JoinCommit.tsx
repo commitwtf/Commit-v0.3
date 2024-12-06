40da2993ce24907db757207a1adf6a3183b58d50
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-12-06 11:40:44 +0100

    Add Swap to mobile sidebar

--- src/components/JoinCommit.tsx
+++ src/components/JoinCommit.tsx
@@ -2,7 +2,12 @@
 import { Button } from '@/components'
 import { TokenAmount } from '@/components/TokenAmount'
 import { COMMIT_CONTRACT_ADDRESS } from '@/config/contract'
-import { useCommitmentToken, useJoinCommitment, useProtocolJoinFee } from '@/hooks/useCommit'
+import {
+  useCommitmentToken,
+  useGetCommitmentDeadlines,
+  useJoinCommitment,
+  useProtocolJoinFee,
+} from '@/hooks/useCommit'
 import { useAllowance, useApprove } from '@/hooks/useToken'
 import { useAccount } from 'wagmi'
 import { Address, formatEther } from 'viem'
@@ -12,6 +17,9 @@ import { ExternalLink } from 'lucide-react'
 import { CheckBalance } from '@/components/CheckBalance'
 import { useUpdateQueries } from '@/hooks/useUpdateQueries'
 
+function hasPassed(date: number) {
+  return Date.now() > date
+}
 export function JoinCommitmentButton({
   commitId,
   participants,
@@ -29,23 +37,30 @@ export function JoinCommitmentButton({
   const { mutateAsync, isPending } = useJoinCommitment()
   const updateQueries = useUpdateQueries()
   const { data: protocolFee } = useProtocolJoinFee()
+  const { data: deadlines } = useGetCommitmentDeadlines(commitId)
 
   const allowance = useAllowance(token!, address!, COMMIT_CONTRACT_ADDRESS)
   const approve = useApprove(token!, COMMIT_CONTRACT_ADDRESS)
   const transferAmount = stakeAmount?.value + creatorFee?.value
 
+  if (deadlines?.[0] && hasPassed(deadlines?.[0])) return <div>Join deadline has passed</div>
+
+  console.log(deadlines, deadlines?.[0] && new Date(deadlines?.[0]))
   if (participants?.includes(address!))
-    return <div className='flex justify-center'>Already joined — complete your commit on{' '}
-      <Link
-        href='https://phi.box'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='ml-1 font-medium underline hover:text-green-700 dark:hover:text-green-300 inline-flex items-center'
-      >
-        Phi
-        <ExternalLink className='w-3 h-3 ml-1' />
-      </Link>
-    </div>
+    return (
+      <div className='flex justify-center'>
+        Already joined — complete your commit on{' '}
+        <Link
+          href='https://phi.box'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='ml-1 font-medium underline hover:text-green-700 dark:hover:text-green-300 inline-flex items-center'
+        >
+          Phi
+          <ExternalLink className='w-3 h-3 ml-1' />
+        </Link>
+      </div>
+    )
 
   if ((allowance.data ?? 0) < stakeAmount?.value)
     return (

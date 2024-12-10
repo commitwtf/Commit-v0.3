5c3949479dd89d59a8b3c60068d1a5a05b62dcd2
revmiller <revmiller@pm.me>
2024-12-10 03:02:56 +0100

    style: remaining Rewards

--- src/data/rewards.ts
+++ src/data/rewards.ts
@@ -3,6 +3,8 @@ export interface Reward {
   description: string
   tokens: number
   requirement: number
+  remainingSlots?: number
+  totalSlots?: number
 }
 export const rewards: Reward[] = [
   {
@@ -10,12 +12,16 @@ export const rewards: Reward[] = [
     description: 'Collect a minimum of 2 Creds from the Phi: Cyber Safari campaign',
     tokens: 1385,
     requirement: 2,
+    remainingSlots: 1559,
+    totalSlots: 3000
   },
   {
     name: 'Medium Level Commit',
     description: 'Collect a minimum of 4 Creds from the Phi: Cyber Safari campaign',
     tokens: 4155,
     requirement: 4,
+    remainingSlots: 1457,
+    totalSlots: 2000
   },
   {
     name: 'Ultimate Level Commit',
